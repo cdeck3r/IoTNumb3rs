72bc4310fe35864b149fe809b31e6391e4a47755
cdeck3r <cdecker@outlook.de>
2018-11-07 21:51:49 +0100

    Vollständige Testversion des Backups fuer den testuser

--- numb3rspipeline/bck_numb3rs.sh
+++ numb3rspipeline/bck_numb3rs.sh
@@ -10,8 +10,22 @@
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPT_DIR
 
+# specific parsing func
+# called by ./funcs.sh/parse_urlfilelist()
+#
+# Param #1: data line from url_filelist.csv
+RET_PARSE_URLFILELIST=()
+parse_urlstr() {
+    URL_STR=$1
+    if [ -n "$URL_STR" ]; then
+        ethercalc_url=$(echo "$URL_STR" | cut -d ';' -f4)
+        RET_PARSE_URLFILELIST+=( "$ethercalc_url" )
+    fi
+}
+
 # include common funcs
 source ./funcs.sh
+
 
 #
 # vars and params
@@ -93,17 +107,7 @@ $GIT config user.email "cdecker@outlook.de"
 
 log_echo "INFO" "All preps done for branch <iotdata> in directory: "$DATAROOT""
 
-# test push using github token
-echo "Date: $(date '+%Y-%m-%d %H:%M:%S,%s')" >> README.md
-$GIT add *
-$GIT commit -m "Testing push using github personal access token"
-$GIT push
-$GIT remote set-url --push origin "https://github.com/cdeck3r/IoTNumb3rs.git"
-
-
 cd "$SCRIPT_DIR"
-
-exit 0
 
 # prep done.
 
@@ -112,28 +116,73 @@ exit 0
 # create & goto into DATAPATH (= user specific dir)
 DATAPATH="$DATAROOT"/"$DROPBOX_USERDIR"
 mkdir -p "$DATAPATH"
-log_echo "INFO" "Create user data directory: "$DATAPATH""
+cd "$DATAPATH"
+log_echo "INFO" "Processing user data directory: "$DATAPATH""
 
 ##################################
 
 # 1.
 # Search for user's url_filelist.csv
-mapfile -t FILELIST < <( "$DB_UPLOADER" search url_filelist.csv )
+mapfile -t FILELIST < <( "$SCRIPT_DIR/$DB_UPLOADER" search url_filelist.csv )
 
 # loop through each element of list
 ALL_URL_FILELIST=()
 for LIST_ELEMENT in "${FILELIST[@]}"
 do
     URL_FILELIST=
-    #URL_FILELIST==$(echo "${LIST_ELEMENT}" | egrep -o '/"$DROPBOX_USERDIR"/[[:digit:]]{8}-[[:digit:]]{4}/url_filelist.csv$' )
     URL_FILELIST=$(echo "${LIST_ELEMENT}" | egrep -o "/$DROPBOX_USERDIR"'/[[:digit:]]{8}-[[:digit:]]{4}/url_filelist.csv$' )
 
     if [ -n "$URL_FILELIST" ]; then
         ALL_URL_FILELIST+=( "$URL_FILELIST" )
     fi
 done
-echo "${ALL_URL_FILELIST[@]}"
-
+#echo "${ALL_URL_FILELIST[@]}"
 # 2.
 # loop through the Ethercalc URLs of each url_filelist.csv
 # ...
+SORTED_FILELIST=()
+SORTED_FILELIST="$(echo "${ALL_URL_FILELIST[@]}" | sort)"
+#echo "${SORTED_FILELIST[@]}"
+for LIST_ELEMENT in "${ALL_URL_FILELIST[@]}"
+do
+    URL_FILELIST="$LIST_ELEMENT"
+
+    # download from dropbox
+    log_echo "INFO" "Download from dropbox "$URL_FILELIST""
+    rm -rf "$DATAPATH"/url_filelist.csv
+    "$SCRIPT_DIR/$DB_UPLOADER" download "$URL_FILELIST" ""$DATAPATH"/url_filelist.csv"
+
+    # parse url_filelist.csv
+    RET_PARSE_URLFILELIST=()
+    #URL_FILELIST_HEADER="url;filename;home_url;ethercalc_url"
+    parse_urlfilelist "${DATAPATH}/url_filelist.csv" "url;filename;home_url;ethercalc_url"
+    if [ "${RET_PARSE_URLFILELIST[0]}" == "ERROR" ]; then
+        # next file
+        continue
+    fi
+
+    # # backup each ethercalc documents in various formats
+    for EC_URL in "${RET_PARSE_URLFILELIST[@]}"
+    do
+        curl -L -k -J -On "$EC_URL.csv"
+        curl -L -k -J -On "$EC_URL.xlxs"
+        curl -L -k -J -On "$EC_URL.md"
+    done
+    # clear DATAPATH dir from url_filelist.csv
+    rm -rf "$DATAPATH"/url_filelist.csv
+done
+
+# clear DATAPATH dir from leftovers
+rm -rf "$DATAPATH"/url_filelist.csv
+
+# push using github token
+echo " " >> "$DATAROOT"/README.md
+echo "Date: $(date '+%Y-%m-%d %H:%M:%S,%s');" \
+    "User: $DROPBOX_USERDIR;" \
+    "Files: $(ls -l $DATAPATH | wc -l )" >> "$DATAROOT"/README.md
+cd "$DATAROOT"
+$GIT add *
+$GIT commit -m "Testing push using github personal access token"
+$GIT push
+# revert to original URL in order to avoid token to be stored
+$GIT remote set-url --push origin "https://github.com/cdeck3r/IoTNumb3rs.git"
--- numb3rspipeline/funcs.sh
+++ numb3rspipeline/funcs.sh
@@ -18,3 +18,47 @@ log_echo () {
     TS=$(date '+%Y-%m-%d %H:%M:%S,%s')
     echo "$TS - $SCRIPT_NAME - $LOG_LEVEL - $LOG_MSG"
 }
+
+# Param #1: path to url_filelist.csv
+# Param #2: header of url_filelist.csv
+parse_urlfilelist() {
+    URL_FILELIST=$1
+    URL_FILELIST_HEADER=$2
+    # Ex. URL_FILELIST_HEADER="url;filename;home_url;ethercalc_url"
+    ERROR=0
+    #check url_filelist.csv format
+    LINE_CNT=0 # run variable
+    while IFS='' read -r URL_STR || [[ -n "$URL_STR" ]]; do
+        #
+        # some preps
+        # removes newline
+        URL_STR=$(echo "$URL_STR" | tr -d '\n' | tr -d '\r')
+        # test if URL_STR is empty (= fake lines)
+        if [[ -z "$URL_STR" ]]; then
+            continue
+        fi
+        # 1st true line of url_filelist is just the header
+        # restart loop
+        LINE_CNT=$((LINE_CNT + 1))
+        if [ $LINE_CNT -eq 1 ]; then
+            # file format check
+            if [ "$URL_STR" == "$URL_FILELIST_HEADER" ]; then
+                log_echo "INFO" "File format of url_filelist.csv is OK."
+                ERROR=0
+            else
+                log_echo "ERROR" "Unexpected file format in url_filelist.csv: "$URL_FILELIST""
+                RET_PARSE_URLFILELIST+=( "ERROR" )
+                ERROR=1
+            fi
+            # proceed with next line
+            continue
+        fi
+
+        # processing and error handling
+        if [[ $ERROR -ne 0 ]]; then
+            break # we stop Processing of url_filelist.csv
+        else
+            parse_urlstr "$URL_STR"
+        fi
+    done < "$URL_FILELIST"
+}
